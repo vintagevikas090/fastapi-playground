7fc5e702ff9c56db52942d75a6719423d388f9a8
Vikas Prajapat <prajapatvikas5005@gmail.com>
2026-05-26 15:10:03 +0530

    setup of fastapi main.py and database connection

---
+++ 01_todo_api/app/database/connection.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+load_dotenv()
+
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+LocalSession = sessionmaker(
+    bind = engine,
+    autocommit = False, 
+    autoflush= False
+)
+
+# try:
+#     with engine.connect():
+#         print('Database Connection Successful')
+# except Exception as e:
+#     print(f"Following Exception Occured")
+#     print(e)
