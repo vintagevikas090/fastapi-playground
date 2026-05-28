857e4c48a4cfc62deb6dedb44282d800a8293750
Vikas Prajapat <prajapatvikas5005@gmail.com>
2026-05-28 12:50:06 +0530

    added two new columns (completed, created_at) in table

--- 01_todo_api/app/database/tables.py
+++ 01_todo_api/app/database/tables.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from app.database.connection import engine
+from datetime import datetime
 
 
 Base = declarative_base()
@@ -12,5 +13,7 @@ class Task(Base):
     title = Column(String(50), nullable=False)
     description = Column(String(250), nullable=False)
     priority = Column(String(10), default='Medium')
+    completed = Column(Boolean, default = False)
+    created_at = Column(DateTime, nullable= False, default = datetime.now())
 
 Base.metadata.create_all(bind = engine)
