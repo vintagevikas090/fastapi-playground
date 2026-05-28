ba48b66d7a62e62e6fb2f632b0a513e7d92eb980
Vikas Prajapat <prajapatvikas5005@gmail.com>
2026-05-28 12:48:37 +0530

    added pydantic class for update task endpoint

--- 01_todo_api/app/schema/task_schema.py
+++ 01_todo_api/app/schema/task_schema.py
@@ -4,3 +4,10 @@ class CreateTask(BaseModel):
     title : str
     description : str
     priority : str
+
+
+class UpdateTask(BaseModel):
+    title : str
+    description : str
+    priority : str
+    completed : bool
