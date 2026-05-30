dd1bf1efbe2136583b0ea652947030d3981cff93
Vikas Prajapat <prajapatvikas5005@gmail.com>
2026-05-30 12:41:57 +0530

    Added data validation and variable metadata for both pydantic classes

--- 01_todo_api/app/schema/task_schema.py
+++ 01_todo_api/app/schema/task_schema.py
@@ -1,13 +1,47 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated, Literal
 
 class CreateTask(BaseModel):
-    title : str
-    description : str
-    priority : str
+    title : Annotated[str, Field(
+        min_length=5, title = 'Task Title', 
+        description='Enter the Title Name for the Task(minimum 5 Characters)', 
+        example = 'GenAI Seminar'
+    )]
+
+    description : Annotated[str, Field(
+        min_length=10, title='Task Description', 
+        description="Enter Some Description for task (minimum 10 Characters)",
+        example = 'Attend GenAI Seminar on 15 June at 5:00 PM'
+    )]
+
+    priority : Annotated[Literal['Low', 'Medium', 'High'], Field(
+        title = 'Task Priority', 
+        description='Enter Task Priority(Low, High, Medium)',
+        examples= ['Low', 'Medium', 'High']
+    )]
 
 
 class UpdateTask(BaseModel):
-    title : str
-    description : str
-    priority : str
-    completed : bool
+    title : Annotated[str, Field(
+        min_length=5, title = 'Task Title', 
+        description='Enter the Title Name for the Task(minimum 5 Characters)', 
+        example = 'GenAI Seminar'
+    )]
+
+    description : Annotated[str, Field(
+        min_length=10, title='Task Description', 
+        description="Enter Some Description for task (minimum 10 Characters)",
+        example = 'Attend GenAI Seminar on 15 June at 5:00 PM'
+    )]
+
+    priority : Annotated[Literal['Low', 'Medium', 'High'], Field(
+        title = 'Task Priority', 
+        description='Enter Task Priority(Low, High, Medium)',
+        examples= ['Low', 'Medium', 'High']
+    )]
+
+    completed: Annotated[bool, Field(
+        title="Task Status",
+        description="Task completion status"
+    )
+    ] = False
